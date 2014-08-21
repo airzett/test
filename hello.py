41a514245e75d8136398c3986d3d8d8ae0e6cf40
airzett <online@airzett.de>
2014-08-21 12:55:39 +0200

    added time current time output

--- hello.py
+++ hello.py
@@ -2,8 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # this file prints out "Hello World!"
+# and the current time
+from datetime import datetime
+
+
 def main():
-	print "Hello World!"
+	print "-------------------------------------------"
+	print datetime.now(), "--> Hello World!"
+	print "-------------------------------------------"
+	
 
 if __name__ == '__main__':
 	main()  
